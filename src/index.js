8fb4247eaafe6e707d17db92d66f4e23ba2f4e03
BR-Jayanth <jayanthbychana@gmail.com>
2024-04-22 18:32:14 +0530

    deployment

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {BrowserRouter} from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL="/To-do-List"}>
+    <BrowserRouter basename={"/To-do-List"}>
     <App />
     </BrowserRouter>
   </React.StrictMode>
