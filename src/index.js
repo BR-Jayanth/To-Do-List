1649e57eced83ad98693f25ca4de52bbac6a1fc5
BR-Jayanth <jayanthbychana@gmail.com>
2024-04-25 11:23:03 +0530

    fixes

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-// import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     {/* <BrowserRouter basename="/To-do-List"> */}
     {/* <BrowserRouter basename={"/To-do-List"}> */}
     <App />
-    {/* </BrowserRouter> */}
+    </BrowserRouter>
   </React.StrictMode>
 );
 
